b1cfd5ec212ecad1c60a1233e93a898d6e8f8e82
dev-ana-lopes <dev.anajulia@outlook.com.br>
2022-09-15 00:24:12 -0300

    finalizando estrutura do projeto

--- programa.py
+++ programa.py
@@ -1,43 +1,84 @@
 from time import sleep
 
-cadastroAlunos = []
-
-def incializacao():
-    nome = input('informe seu nome: ')
-    return nome
+lista = []
+    
+def inicializacao():
+    solicitante = input('Informe seu nome: ')
+    print('Olá, ' + solicitante + '. Você está no sistema Jullies Education!!')
 
 def chamaMenu(): 
-    solicitante = incializacao
-    print('Olá, ' + solicitante + '. Você está no sistema Jullies Education!! \n\nPara o menu: \n(1) Cadastrar Aluno \n(2) Editar Aluno \n(3) Visualizar Aluno \n(4) Excluir Aluno \n(5) Sair \n')
+    sleep(2)
+    print('\nPara o menu: \n(1) Cadastrar Aluno \n(2) Editar Aluno \n(3) Visualizar Aluno \n(4) Excluir Aluno \n(5) Sair \n')
+    sleep(2)
     comando = input('Informe o digito de acesso: ')
-    return comando
+    return int(comando)
 
 def executaComando():
-    solicitacao = int(chamaMenu())
-    
+    solicitacao = chamaMenu()
+
     if solicitacao == 1:
 
-        crm = input('Informe o crm: ')
+        crm = (input('Informe o crm: '))
         nome = input('Informe o nome: ')
         curso = input('Informe o curso: ')
         aluno = {'crm': crm, 'nome': nome, 'curso': curso}
-        #return lista.append(aluno,)
+        lista.append(aluno)
+        
+        return print('Aluno cadastrado')
         
     elif solicitacao == 2:
-        pass
+        crm = input('Informe o crm do aluno que deseja altera: ')
+        find = next(x for x in lista if x["crm"] == crm)
+        index = lista.index(find)
+        nome = input('Informe o nome: ')
+        curso = input('Informe o curso: ')
+        lista[index] = {"nome": nome, "curso": curso}
+        
+        return print('Aluno alterado')
+
     elif solicitacao == 3:
-        pass
+        crm = input('Informe o crm do aluno que deseja visualizar: ')
+        find = next(x for x in lista if x["crm"] == crm)
+        print('Aluno encontrado\n')
+        sleep(1)
+        print(find)
+        sleep(1)
+
     elif solicitacao == 4:
-        pass
+        crm = input('Informe o crm do aluno que deseja deletar: ')
+        find = next(x for x in lista if x["crm"] == crm)
+        index = lista.index(find)
+        lista.pop(index)
+        return print('Aluno apagado')
+
     elif solicitacao == 5:
+        
         print('Até logo...')
+        
+        return exit
+        
     else:
         sleep(1)
         print('\nO número inserido não está de acordo com o menu. Tente Novamente!!\n')
         sleep(1)
-        chamaMenu()
+      
     
 if __name__ == '__main__': 
     
-    pass
+    inicializacao()
+    
+    while True:
+        
+        if executaComando() == exit:
+            
+            break
+        
+        else:
+            executaComando()
+        
+        
+    
+    
+
+
         
